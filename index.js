aef2fe56ad6f450f685c2aaad46c29562893f3d4
Annes Bani Al Doumi <annesdoumi2@gmail.com>
2024-10-06 21:02:51 +0200

    index.html ansprechen statt hallo welt, in der index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+// Stelle den public Ordner für statische Dateien zur Verfügung
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('Hallo Welt!');
+    // Sende die index.html an den Client
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
